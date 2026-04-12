68e0df27e5e705876778f6c329da7873cd69b151
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-04-12 22:22:15 +0200

    created transaction entity

---
+++ domain/model/transaction.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+namespace budgetpilot::domain::model {
+    enum class Type : std::uint8_t {
+        INCOME = 1,
+        EXPENSE = 2
+    };
+
+    struct Transaction {
+        std::int64_t id;
+        std::string source = "-";
+        Type type = Type::INCOME;
+        float amount;
+        std::int64_t category_id;
+    };
+}
