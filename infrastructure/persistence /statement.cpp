a7875474c3f3d3fd8dd802c041cc13b5bcf26758
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-04-17 23:59:56 +0200

    .

--- infrastructure/persistence /statement.cpp
+++ infrastructure/persistence /statement.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Daniel Hadzhiev on 17.04.26.
-//
+#include "statement.hpp"
+
+#include <stdexcept>
+
+namespace budgetpilot::infrastructure::persistence {
+    Statement::Statement(sqlite3 *db, const char *sql)
+        : stmt_(nullptr) {
+        int result = sqlite3_prepare_v2(db, sql, -1, &stmt_, nullptr);
+        if (result != SQLITE_OK) {
+            throw std::runtime_error(sqlite3_errmsg(db));
+        }
+    }
+
+    Statement::~Statement() {
+        if (stmt_) {
+            sqlite3_finalize(stmt_);
+            stmt_ = nullptr;
+        }
+    }
+
+    sqlite3_stmt *Statement::get() const {
+        return stmt_;
+    }
+}
--- infrastructure/persistence /statement.hpp
+++ infrastructure/persistence /statement.hpp
@@ -1,8 +1,22 @@
-//
-// Created by Daniel Hadzhiev on 17.04.26.
-//
+#pragma once
+#include <sqlite3.h>
 
-#ifndef BUDGETPILOT_STATEMENT_HPP
-#define BUDGETPILOT_STATEMENT_HPP
 
-#endif //BUDGETPILOT_STATEMENT_HPP
+namespace budgetpilot::infrastructure::persistence {
+    class Statement {
+    public:
+        explicit Statement(sqlite3 *connection, const char *sql);
+        ~Statement();
+
+        Statement(const Statement &) = delete;
+        Statement &operator =(const Statement &) = delete;
+
+        Statement(Statement &&) = delete;
+        Statement &&operator =(Statement &&) = delete;
+
+        [[nodiscard]] sqlite3_stmt *get() const;
+
+    private:
+        sqlite3_stmt *stmt_;
+    };
+}
