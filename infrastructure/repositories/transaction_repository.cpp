114ed423eff3ec522274abef5ab79428a1f1de55
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-04-18 00:10:46 +0200

    add update logic

--- infrastructure/repositories/transaction_repository.cpp
+++ infrastructure/repositories/transaction_repository.cpp
@@ -30,6 +30,27 @@ namespace budgetpilot::infrastructure::repositories {
         }
     }
 
+    void TransactionRepository::update(const domain::model::Transaction &transaction) {
+        const char *sql = R"(
+        UPDATE transactions
+        SET amount = ?, type = ?, source = ?, category_id = ?
+        WHERE id = ?
+    )";
+
+        persistence::Statement stmt(connection_, sql);
+
+        sqlite3_bind_double(stmt.get(), 1, transaction.amount);
+        sqlite3_bind_int(stmt.get(), 2, static_cast<int>(transaction.type));
+        sqlite3_bind_text(stmt.get(), 3, transaction.source.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_int(stmt.get(), 4, static_cast<int>(transaction.category_id));
+        sqlite3_bind_int64(stmt.get(), 5, static_cast<sqlite3_int64>(transaction.id));
+
+        int result = sqlite3_step(stmt.get());
+        if (result != SQLITE_DONE) {
+            throw std::runtime_error(sqlite3_errmsg(connection_));
+        }
+    }
+
     void TransactionRepository::remove(std::uint64_t id) {
         const char *sql = R"(
                              DELETE FROM transactions
