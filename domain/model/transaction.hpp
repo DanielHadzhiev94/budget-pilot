40ea32de471ff29e2b492a23fab2e5ef2f6682e9
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-04-18 00:10:33 +0200

    ctor without id

--- domain/model/transaction.hpp
+++ domain/model/transaction.hpp
@@ -8,9 +8,20 @@ namespace budgetpilot::domain::model {
     };
 
     struct Transaction {
+        std::int64_t id = 0 ;
         std::string source = "-";
         Type type = Type::INCOME;
         float amount;
         std::int64_t category_id;
+
+        Transaction(std::string source,
+                Type type,
+                float amount,
+                std::int64_t category_id)
+        : source(std::move(source)),
+          type(type),
+          amount(amount),
+          category_id(category_id)
+        {}
     };
 }
