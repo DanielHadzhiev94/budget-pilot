e11f2155d7992632c51318e8c52467c723da355a
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-04-19 23:23:49 +0200

    add amount

--- domain/model/account.hpp
+++ domain/model/account.hpp
@@ -6,6 +6,7 @@ namespace budgetpilot::domain::model {
     struct Account {
         std::uint64_t id{0};
         std::string name{};
+        double amount{0.0};
 
         Account() {
             if (name.empty() != 1) {
