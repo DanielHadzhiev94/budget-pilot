16535732b1c1e03006eae40479c037374cd81f29
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-05-05 23:09:30 +0200

    added void functionality for response which should only return message

--- src/domain/utilities/Reponse.hpp
+++ src/domain/utilities/Reponse.hpp
@@ -20,7 +20,7 @@ namespace budgetpilot::domain::utilities {
         std::string message() const { return message_; } // NOLINT(*-const-return-type, *-use-nodiscard)
 
         const T &data() const {
-            if (!data_.has_value) {
+            if (!data_.has_value()) {
                 throw std::runtime_error("Data has now value");
             }
 
@@ -35,5 +35,34 @@ namespace budgetpilot::domain::utilities {
         Response(std::optional<T> data, std::string message, const bool is_successful)
             : data_(std::move(data)), message_(std::move(message)), is_successful_(is_successful) {
         };
+    };
+
+    template<>
+    class Response<void> {
+    public:
+        static Response<void> Success(std::string message = "") {
+            return Response<void>(std::move(message), true);
+        }
+
+        static Response<void> Failed(std::string message) {
+            return Response<void>(std::move(message), false);
+        }
+
+        [[nodiscard]] bool is_successful() const {
+            return is_successful_;
+        }
+
+        [[nodiscard]] const std::string &message() const {
+            return message_;
+        }
+
+    private:
+        std::string message_;
+        bool is_successful_;
+
+        Response(std::string message, bool is_successful)
+            : message_(std::move(message)),
+              is_successful_(is_successful) {
+        }
     };
 }
